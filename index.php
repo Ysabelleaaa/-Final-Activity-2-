236f9d8a5aecb0332369a78ded3de4b16c764866
Ysabelleaaa <jmgadot@paterostechnologicalcollege.edu.ph>
2026-04-27 22:07:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,47 +4,10 @@ echo "Activity 1:";
 echo "Name: Jessa Mae N. Gadot <br>";
 print "Course: BSIT <br><br>";
 
-echo "Activity 2:";
-echo "Welcome!<br>";
-echo "This is PHP.<br>";
-echo "Enjoy coding!<br><br>";
-
-echo "Activity 3:";
-$name = "Jessa";
-$age = 19;
-
-echo "Name: " . $name. "<br>";
-echo "Age: " . $age. "<br><br>";
-
-echo "Activity 4:";
-$name = "Jessa";
-$age = 19;
-
-echo "Fixed Age: " . $age . "<br><br>";
-
-echo "Acivity 5:";
-$a = 13;
-$b = 21;
-
-echo "Addition: " . ($a + $b) . "<br>";
-echo "Subtraction: " . ($a - $b) . "<br>";
-echo "Multiplication: " . ($a * $b) . "<br>";
-echo "Division: " . ($a / $b) . "<br><br>";
-
-echo "Activity 6:";
-$x = 8;
-$x += 11;
-
-echo "Assignment Result: " . $x . "<br><br>";
-
-echo "Activity 7:";
-$a = 11;
-$b = 5;
-
-echo "Is a > b? ";
-var_dump($a > $b);
-
-echo "<br>Is a == b? ";
-var_dump($a == $b);
-
 ?>
+
+Guide Questions:
+1. 2. What symbol is used to end a PHP statement?
+answer: Semicolon ( ; )
+Which is faster: echo or print?
+answer: echo is slightly faster than print
